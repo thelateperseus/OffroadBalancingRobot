6676e9a9b28b0ad4deaf1ff484d9e1bd28757e6b
Nathan C <1849077+thelateperseus@users.noreply.github.com>
2020-08-21 18:28:40 +1000

    Functions to read accelerometer and gyro

--- Nano33IMUTest/Nano33IMUTest.ino
+++ Nano33IMUTest/Nano33IMUTest.ino
@@ -4,10 +4,10 @@ const int GYRO_ADDRESS = 0x68;
 const float PITCH_CALIBRATION = 0.34;
 const int LOOP_MICROS = 4000;
 
-int16_t gyroYCalibration = 0;
-int16_t accelerometerXCalibration = 0;
-int16_t accelerometerYCalibration = 0;
-int16_t accelerometerZCalibration = 0;
+float gyroYCalibration = 0;
+float accelerometerXCalibration = 0;
+float accelerometerYCalibration = 0;
+float accelerometerZCalibration = 0;
 long loopEndTime = 0;
 float pitchReading = 0;
 
@@ -21,66 +21,50 @@ void setup() {
   Wire.setClock(1000000);
 
   // Wake up the MPU-6050
-  Wire.beginTransmission(GYRO_ADDRESS);
-  Wire.write(0x6B); // PWR_MGMT_1 register
-  Wire.write(0x00);
-  Wire.endTransmission();
+  writeRegister(0x6B, 0x00); // PWR_MGMT_1 register
   // Set the full scale of the gyro to +/- 250 degrees per second
-  Wire.beginTransmission(GYRO_ADDRESS);
-  Wire.write(0x1B); // GYRO_CONFIG register
-  Wire.write(0x00);                                                         //Set the register bits as 00000000 (250dps full scale)
-  Wire.endTransmission();                                                   //End the transmission with the gyro
+  writeRegister(0x1B, 0x00); // GYRO_CONFIG register
   // Set the full scale of the accelerometer to +/- 2g.
-  Wire.beginTransmission(GYRO_ADDRESS);
-  Wire.write(0x1C); // ACCEL_CONFIG register
-  Wire.write(0x00);
-  Wire.endTransmission();
-  // Enable Digital Low Pass Filter to improve the raw data.
-  Wire.beginTransmission(GYRO_ADDRESS);
-  Wire.write(0x1A); // CONFIG register
-  Wire.write(0x03); // ~43Hz
-  Wire.endTransmission();
+  writeRegister(0x1C, 0x00); // ACCEL_CONFIG register
+  // Enable Digital Low Pass Filter ~43Hz to improve the raw data.
+  writeRegister(0x1A, 0x03); // CONFIG register
 
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.println("Measuring gyro calibration values...");
   loopEndTime = micros() + LOOP_MICROS;
-  long gyroYSum = 0;
-  long accelerometerXSum = 0;
-  long accelerometerYSum = 0;
-  long accelerometerZSum = 0;
+  float gxSum = 0;
+  float gySum = 0;
+  float gzSum = 0;
+  float axSum = 0;
+  float aySum = 0;
+  float azSum = 0;
   for (int i = 0; i < 500; i++) {
     if (i % 15 == 0) {
       digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));        //Change the state of the LED every 15 loops to make the LED blink fast
     }
-    Wire.beginTransmission(GYRO_ADDRESS);
-    Wire.write(0x45); // GYRO_YOUT[15:8]
-    Wire.endTransmission();
-    Wire.requestFrom(GYRO_ADDRESS, 2); // GYRO_YOUT[15:8], GYRO_YOUT[7:0]
-    int16_t gyroY = Wire.read() << 8 | Wire.read();
-    gyroYSum += gyroY;
 
-    // Read x, y, z accelerometer values
-    Wire.beginTransmission(GYRO_ADDRESS);
-    Wire.write(0x3B); // ACCEL_XOUT[15:8]
-    Wire.endTransmission();
-    Wire.requestFrom(GYRO_ADDRESS, 6); // ACCEL_XOUT[15:8], ACCEL_XOUT[7:0], ACCEL_YOUT[15:8], ACCEL_YOUT[7:0], ACCEL_ZOUT[15:8], ACCEL_ZOUT[7:0]
-    int16_t accelerometerX = Wire.read() << 8 | Wire.read();
-    accelerometerXSum += accelerometerX;
-    int16_t accelerometerY = Wire.read() << 8 | Wire.read();
-    accelerometerYSum += accelerometerY;
-    int16_t accelerometerZ = Wire.read() << 8 | Wire.read();
-    accelerometerZSum += accelerometerZ;
+    float gx, gy, gz;
+    readGyro(gx, gy, gz);
+    gxSum += gx;
+    gySum += gy;
+    gzSum += gz;
+
+    float ax, ay, az;
+    readAccelerometer(ax, ay, az);
+    axSum += ax;
+    aySum += ay;
+    azSum += az;
 
     delayMicroseconds(loopEndTime - micros()); // Simulate the main program loop time
     loopEndTime = micros() + LOOP_MICROS;
   }
   // Get the average from the 500 readings
-  gyroYCalibration = gyroYSum / 500;
-  accelerometerXCalibration = accelerometerXSum / 500;
-  accelerometerYCalibration = accelerometerYSum / 500;
-  accelerometerZCalibration = accelerometerZSum / 500;
+  gyroYCalibration = gySum / 500;
+  accelerometerXCalibration = axSum / 500;
+  accelerometerYCalibration = aySum / 500;
+  accelerometerZCalibration = azSum / 500;
 
   // Gyro calibration complete, gy: 275, ax: -138, ay: -286, az: 18227
   Serial.print("Gyro calibration complete, gy: ");
@@ -99,37 +83,28 @@ void setup() {
 
 void loop() {
   // Read x, y, z accelerometer values
-  Wire.beginTransmission(GYRO_ADDRESS);
-  Wire.write(0x3B); // ACCEL_XOUT[15:8]
-  Wire.endTransmission();
-  Wire.requestFrom(GYRO_ADDRESS, 6); // ACCEL_XOUT[15:8], ACCEL_XOUT[7:0], ACCEL_YOUT[15:8], ACCEL_YOUT[7:0], ACCEL_ZOUT[15:8], ACCEL_ZOUT[7:0]
-  int16_t accelerometerX = Wire.read() << 8 | Wire.read();
-  int16_t accelerometerY = Wire.read() << 8 | Wire.read();
-  int16_t accelerometerZ = Wire.read() << 8 | Wire.read();
+  float ax, ay, az;
+  readAccelerometer(ax, ay, az);
 
   // Calculate the current pitch angle in degrees according to the accelerometer
-  //double pitchAccelerometer = asin((double)accelerometerX / 8200.0) * 57.296;
-  float pitchAccelerometer = atan(-1 * accelerometerX / sqrt(pow(accelerometerY, 2) + pow(accelerometerZ, 2))) * 57.296;
+  float pitchAccelerometer = atan(-1 * ax / sqrt(pow(ay, 2) + pow(az, 2))) * 57.296;
   pitchAccelerometer -= PITCH_CALIBRATION;
 
   Serial.print(", pa: ");
   Serial.print(pitchAccelerometer);
 
-  // Read X and Y gyro values
-  Wire.beginTransmission(GYRO_ADDRESS);
-  Wire.write(0x45); // GYRO_YOUT[15:8]
-  Wire.endTransmission();
-  Wire.requestFrom(GYRO_ADDRESS, 2); // GYRO_YOUT[15:8], GYRO_YOUT[7:0]
-  int16_t gyroYRaw = Wire.read() << 8 | Wire.read();
+  // Read gyro values
+  float gx, gy, gz;
+  readGyro(gx, gy, gz);
 
-  // Convert to degrees per second
-  float gyroY = (gyroYRaw - gyroYCalibration) / 131.0;
+  // Apply calibration value
+  gy -= gyroYCalibration;
 
   float dt = LOOP_MICROS / 1000000.0;
 
   // Calculate the angle in degrees traveled during this loop angle
   // (Gyroscope Angle) = (Last Measured Filtered Angle) + ω×Δt
-  float pitchGyro = pitchReading + gyroY * dt;
+  float pitchGyro = pitchReading + gy * dt;
 
   // Complementary filter to combine the gyro and accelerometer angle
   // Filtered Angle = α × (Gyroscope Angle) + (1 − α) × (Accelerometer Angle)
@@ -143,4 +118,39 @@ void loop() {
   // The angle calculations are tuned for a loop time of 4 milliseconds
   delayMicroseconds(loopEndTime - micros()); // Simulate the main program loop time
   loopEndTime = micros() + LOOP_MICROS;
+}
+
+void writeRegister(byte address, byte value) {
+  Wire.beginTransmission(GYRO_ADDRESS);
+  Wire.write(address);
+  Wire.write(value);
+  Wire.endTransmission();
+}
+
+// Results are in g (earth gravity)
+void readAccelerometer(float& ax, float& ay, float& az) {
+  Wire.beginTransmission(GYRO_ADDRESS);
+  Wire.write(0x3B); // ACCEL_XOUT[15:8]
+  Wire.endTransmission();
+  Wire.requestFrom(GYRO_ADDRESS, 6); // ACCEL_XOUT[15:8], ACCEL_XOUT[7:0], ACCEL_YOUT[15:8], ACCEL_YOUT[7:0], ACCEL_ZOUT[15:8], ACCEL_ZOUT[7:0]
+  int16_t axRaw = Wire.read() << 8 | Wire.read();
+  ax = axRaw / 16384.0;
+  int16_t ayRaw = Wire.read() << 8 | Wire.read();
+  ay = ayRaw / 16384.0;
+  int16_t azRaw = Wire.read() << 8 | Wire.read();
+  az = azRaw / 16384.0;
+}
+
+// Results are in degrees per second
+void readGyro(float& gx, float& gy, float& gz) {
+  Wire.beginTransmission(GYRO_ADDRESS);
+  Wire.write(0x43); // GYRO_XOUT[15:8]
+  Wire.endTransmission();
+  Wire.requestFrom(GYRO_ADDRESS, 6); // GYRO_XOUT[15:8], GYRO_XOUT[7:0], GYRO_YOUT[15:8], GYRO_YOUT[7:0], GYRO_ZOUT[15:8], GYRO_ZOUT[7:0]
+  int16_t gxRaw = Wire.read() << 8 | Wire.read();
+  gx = gxRaw / 131.0;
+  int16_t gyRaw = Wire.read() << 8 | Wire.read();
+  gy = gyRaw / 131.0;
+  int16_t gzRaw = Wire.read() << 8 | Wire.read();
+  gz = gzRaw / 131.0;
 }
