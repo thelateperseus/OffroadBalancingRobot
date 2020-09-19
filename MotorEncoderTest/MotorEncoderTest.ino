c59a20790cd1f49dda4883a43f560ebaacaab982
Nathan C <1849077+thelateperseus@users.noreply.github.com>
2020-09-19 14:01:07 +1000

    Loop through all test PWM values
    Also print the PWM, the measurement interval and the encoder pulse count for easy import into Excel.

--- MotorEncoderTest/MotorEncoderTest.ino
+++ MotorEncoderTest/MotorEncoderTest.ino
@@ -5,11 +5,15 @@ const int IN1 = 5;
 const int IN2 = 7;
 const int PWM = 6;
 
-const int MOTOR_SPEED = 12;
+const int MOTOR_SPEEDS[] = { 0, 8, 12, 16, 24, 32, 48, 64, 96, 128, 160, 192, 224, 255 };
+const int MOTOR_SPEEDS_LENGTH = 14;
 const int LOOP_MICROS = 1000000;
 
 volatile int pulseCount = 0;
 long loopEndTime = 0;
+long measurementTime = 0;
+int speedIndex = 0;
+int speedLoopCount = 0;
 
 void setup() {
   pinMode(IN1, OUTPUT);
@@ -18,13 +22,12 @@ void setup() {
 
   Serial.begin(2000000);
   while (!Serial);
-  Serial.print("PWM Value: ");
-  Serial.println(MOTOR_SPEED);
 
-  pinMode(ENCODER_A, INPUT);
-  pinMode(ENCODER_B, INPUT);
+  pinMode(ENCODER_A, INPUT_PULLUP);
+  pinMode(ENCODER_B, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(ENCODER_A), encoderPulse, RISING);
 
+  measurementTime = micros();
   loopEndTime = micros() + LOOP_MICROS;
 }
 
@@ -33,22 +36,39 @@ void encoderPulse() {
 }
 
 void loop() {
+  if (speedIndex >= MOTOR_SPEEDS_LENGTH) {
+    analogWrite(PWM, 0);
+    while (true);
+  }
+
+  int speed = MOTOR_SPEEDS[speedIndex];
+  speedLoopCount++;
+  if (speedLoopCount >= 15) {
+    speedIndex++;
+    speedLoopCount = 0;
+  }
+
   //control speed 
-  analogWrite(PWM, MOTOR_SPEED);
+  analogWrite(PWM, speed);
   //control direction 
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
 
   noInterrupts();
+  long now = micros();
   int pulseCountValue = pulseCount;
   pulseCount = 0;
   interrupts();
 
+  long measurementInterval = now - measurementTime;
+  measurementTime = now;
+
+  Serial.print(speed);
+  Serial.print(",");
+  Serial.print(measurementInterval);
+  Serial.print(",");
   Serial.println(pulseCountValue);
 
-  long delayTime = loopEndTime - micros();
-  if (delayTime > 0) {
-    delayMicroseconds(delayTime);
-  }
-  loopEndTime = micros() + LOOP_MICROS;
+  while(loopEndTime > micros());
+  loopEndTime += LOOP_MICROS;
 }
