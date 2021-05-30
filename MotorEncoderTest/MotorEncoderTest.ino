783651d2c51e80ce5cc54f43b754eeeba7b0f719
Nathan C <1849077+thelateperseus@users.noreply.github.com>
2021-05-30 15:45:13 +1000

    TurboPWM library (20kHz) and deadband testing

--- MotorEncoderTest/MotorEncoderTest.ino
+++ MotorEncoderTest/MotorEncoderTest.ino
@@ -1,3 +1,4 @@
+#include <SAMD21turboPWM.h>
 #include "Encoder.h"
 
 const int ENCODER_A_L = 9;
@@ -12,8 +13,11 @@ const int INA_R = A2;
 const int INB_R = A3;
 const int PWM_R = 6;
 
-const int MOTOR_SPEEDS[] = { 16, 20, 24, 32, 48, 64, 96, 128, 160, 192, 224, 255 };
-const int MOTOR_SPEEDS_LENGTH = 12;
+//const int MOTOR_SPEEDS[] = { 16, 20, 24, 32, 48, 64, 96, 128, 160, 192, 224, 255 };
+//const int MOTOR_SPEEDS_LENGTH = 12;
+//const int MOTOR_SPEEDS[] = { 8, 12, 16, 20, 24, 32 };
+const int MOTOR_SPEEDS[] = { 7, 8, 9, 10, 16, 20, 24, 32 };
+const int MOTOR_SPEEDS_LENGTH = 8;
 const int LOOP_MICROS = 1000000;
 
 long loopEndTime = 0;
@@ -21,6 +25,7 @@ long measurementTime = 0;
 int speedIndex = 0;
 int speedLoopCount = 0;
 
+TurboPWM pwm;
 Encoder encoderLeft(ENCODER_A_L, ENCODER_B_L);
 Encoder encoderRight(ENCODER_A_R, ENCODER_B_R);
 
@@ -56,35 +61,48 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(ENCODER_A_R), encoderRightInterruptA, CHANGE);
   attachInterrupt(digitalPinToInterrupt(ENCODER_B_R), encoderRightInterruptB, CHANGE);
 
+  // Turbo = true
+  pwm.setClockDivider(1, false);
+  // For the Arduino Nano 33 IoT, you need to initialise timer 1 for pins 4 and 7, timer 0 for pins 5, 6, 8, and 12, and timer 2 for pins 11 and 13;
+  // timer 1, prescaler 1, resolution 4800, fast PWM
+  pwm.timer(0, 1, 1200, false);
+  Serial.println(pwm.frequency(0));
+  //analogWriteResolution(10);
+
   measurementTime = micros();
   loopEndTime = micros() + LOOP_MICROS;
 }
 
 void loop() {
   if (speedIndex >= MOTOR_SPEEDS_LENGTH) {
-    analogWrite(PWM_L, 0);
-    analogWrite(PWM_R, 0);
+    pwm.analogWrite(PWM_L, 0);
+    pwm.analogWrite(PWM_R, 0);
     while (true);
   }
 
-  int speed = 40; //MOTOR_SPEEDS[speedIndex];
-  /*speedLoopCount++;
+  // Deadband left: 11-12, right: 9-10
+  int speed = 12;
+  //int speed = MOTOR_SPEEDS[speedIndex];
+  speedLoopCount++;
   if (speedLoopCount >= 10) {
     speedIndex++;
     speedLoopCount = 0;
-  }*/
+  }
+
+  int speedLeft = speed;
+  int speedRight = speed;
 
   //control speed 
-  analogWrite(PWM_L, speed);
+  pwm.analogWrite(PWM_L, abs(speedLeft));
   //control direction 
-  digitalWrite(INA_L, LOW);
-  digitalWrite(INB_L, HIGH);
+  digitalWrite(INA_L, speedLeft > 0 ? LOW : HIGH);
+  digitalWrite(INB_L, speedLeft > 0 ? HIGH : LOW);
 
   //control speed
-  analogWrite(PWM_R, speed);
+  pwm.analogWrite(PWM_R, abs(speedRight));
   //control direction
-  digitalWrite(INA_R, HIGH);
-  digitalWrite(INB_R, LOW);
+  digitalWrite(INA_R, speedRight > 0 ? HIGH : LOW);
+  digitalWrite(INB_R, speedRight > 0 ? LOW : HIGH);
 
   int valueLeft = encoderLeft.getDeltaValue();
   int valueRight = -encoderRight.getDeltaValue();
